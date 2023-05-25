62f208ff085bf763a41dd26b0031d43a6f7ab9d7
PatrickSilvaMenezes <patricksilvamenezes@gmail.com>
2023-05-24 21:27:25 -0300

    refac: add prisma connection

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,2 +1,19 @@
-import app from './config/app'
-app.listen(5050, () => console.log('Server is running on http://localhost:5050'))
+import { PrismaHelper } from "../infra/db/prisma/prisma";
+import { setupApp } from './config/app'
+import { exec } from 'node:child_process'
+
+const main = async () => {
+  const app = await setupApp()
+  const server = app.listen(process.env.PORT || 4040, () => {
+    exec("npx prisma migrate dev --name v4 && npx prisma db push")
+    console.log(`server is running on port ${process.env.PORT}`)
+  })
+  process.on('SIGTERM', () => {
+    server.close(() => {
+      PrismaHelper.$disconnect()
+      process.exit(0)
+    });
+  });
+}
+
+main()
